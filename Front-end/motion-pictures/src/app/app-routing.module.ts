595db90f40ab80d54563576b7adce9b89a537fb0
sharonjames32 <sharonjames19931993@gmail.com>
2022-07-24 18:59:24 -0400

    Added admin login page

--- Front-end/motion-pictures/src/app/app-routing.module.ts
+++ Front-end/motion-pictures/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [];
     RouterModule.forRoot([
       { path: '', component: MovieListComponent },
       { path: 'movie-details', component: MovieDetailComponent },
+      { path: 'login', component: AdminLoginComponent },
     ])],
   exports: [RouterModule]
 })
