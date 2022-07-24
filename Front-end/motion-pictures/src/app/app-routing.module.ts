d05c76abc27b7391fc03c9a5a0180ee025ccc17a
sharonjames32 <sharonjames19931993@gmail.com>
2022-07-24 17:50:29 -0400

    * Added logo icon * UI changes * routing to movie detail page * navigation added in toolbar to movielist page

--- Front-end/motion-pictures/src/app/app-routing.module.ts
+++ Front-end/motion-pictures/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 
 const routes: Routes = [];
@@ -7,7 +8,9 @@ const routes: Routes = [];
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', component: MovieListComponent }])],
+      { path: '', component: MovieListComponent },
+      { path: 'movie-details', component: MovieDetailComponent },
+    ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Front-end/motion-pictures/src/app/movie-list/movie-list.component.ts
+++ Front-end/motion-pictures/src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -18,6 +19,11 @@ export class MovieListComponent implements OnInit {
     }else{
       
     }
+
+  }
+
+  navigate(){
+    this.router.navigateByUrl('movie-details');
 
   }
 
