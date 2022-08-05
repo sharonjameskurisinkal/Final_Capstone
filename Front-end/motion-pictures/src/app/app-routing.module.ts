c92d9e39d09880cdc306afdd23749bb9353c4afe
ancy <ancyjoy28@gmail.com>
2022-08-04 22:39:26 -0400

    Booking page

--- Front-end/motion-pictures/src/app/app-routing.module.ts
+++ Front-end/motion-pictures/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { MovieBookingComponent } from './movie-booking/movie-booking.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 
@@ -12,6 +13,8 @@ const routes: Routes = [];
       { path: '', component: MovieListComponent },
       { path: 'movie-details', component: MovieDetailComponent },
       { path: 'login', component: AdminLoginComponent },
+
+      { path: 'booking', component: MovieBookingComponent },
     ])],
   exports: [RouterModule]
 })
