88d946e637af65d46d09028676eb9bf52725675d
ancy <ancyjoy28@gmail.com>
2022-08-09 16:15:25 -0400

    UI changes and background image update on UI

--- Front-end/motion-pictures/src/app/app-routing.module.ts
+++ Front-end/motion-pictures/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { BookingSummaryComponent } from './booking-summary/booking-summary.component';
 import { MovieBookingComponent } from './movie-booking/movie-booking.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
@@ -15,6 +16,7 @@ const routes: Routes = [];
       { path: 'login', component: AdminLoginComponent },
 
       { path: 'booking', component: MovieBookingComponent },
+      { path: 'pay', component: BookingSummaryComponent },
     ])],
   exports: [RouterModule]
 })
