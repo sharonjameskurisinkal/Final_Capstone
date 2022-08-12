36c679bbafdcd712e347d1e2b179a513ba14d70a
sharonjames32 <sharonjames19931993@gmail.com>
2022-08-12 16:47:13 -0400

    movie-booking changes

--- Front-end/motion-pictures/src/app/movie-booking/movie-booking.component.ts
+++ Front-end/motion-pictures/src/app/movie-booking/movie-booking.component.ts
@@ -8,12 +8,22 @@ import { MovieService } from '../movie-service.service';
 })
 export class MovieBookingComponent implements OnInit {
   public movieDetail:  any;
+  public showSeats = false;
   constructor(private movieService: MovieService) {
     console.log(this.movieService.selectedMovie);
     this.movieDetail = this.movieService.selectedMovie;
    }
 
   ngOnInit(): void {
+  }
+
+  selectShow(selectedShow: any,selectedTheatre: any){
+    console.log(selectedShow);
+    console.log(selectedTheatre);
+    selectedShow.selected = true;
+    selectedTheatre.selected = true;
+    this.showSeats = true;
+
   }
 
 }
